1ee9a513bf40b445d0caddcc53eade331f34bc17
bliang.tw <homebetty0125@gmail.com>
2022-09-22 12:37:43 +0800

    - Init for dev branch

---
+++ src/Frame.js
@@ -0,0 +1,18 @@
+import { Fragment } from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
+const Frame = () => {
+
+    return (
+
+        <Fragment>
+            <h1>Hi</h1>
+            <Button type="primary">Primary Button</Button>
+        </Fragment>
+
+    );
+
+};
+
+export default Frame;
