66a053baadb5e5108cf85f530f50a16fc9359ca5
bliang.tw <homebetty0125@gmail.com>
2022-09-24 17:52:15 +0800

    - Add login page

--- src/containers/GlobalStyle.js
+++ src/containers/GlobalStyle.js
@@ -1,12 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+    html, body {
+        height: auto;
+    }
     body {
         font-size: 1em;
+        color: #333;
+        background-color: #f0f2f5;
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        color: #333;
     }
     h1, h2, h3, h4, h5, h6 {
         margin-bottom: 20px;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,170 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { redirect } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+import styled from 'styled-components';
 
+import ContentHeader from '../containers/ContentHeader';
+import Buttons from '../components/Buttons';
+
+import utilConst from '../utils/util.const';
+import Service from '../utils/util.service';
+
+const { errorMesg } = utilConst;
+
+//
+const MainLayout = styled.main({
+    maxWidth: '400px',
+    backgroundColor: '#FFF',
+    borderRadius: '4px',
+    margin: '120px auto 60px',
+    padding: '60px 40px 80px',
+    'h1': {
+        textAlign: 'center',
+    },
+});
+
+//
+const FormLayout = styled.form({
+    '.btn-admin': {
+        width: '100%',
+        marginTop: '10px',
+        padding: '6px 24px',
+    },
+});
+
+//
+const FormRow = styled.label(({ theme }) => ({
+    display: 'block',
+    marginBottom: '20px',
+    '.title': {
+        marginBottom: '4px',
+    },
+    '.field': {
+        border: `1px solid ${theme.palette.border}`,
+        borderRadius: '2px',
+        padding: '4px 8px',
+    },
+    'input': {
+        width: '100%',
+        height: '100%',
+        border: '0',
+    },
+    '&.hasError': {
+        '.field': {
+            borderColor: 'red',
+            '&:hover': {
+                borderColor: 'red',
+            },
+        },
+        '.error': {
+            fontSize: '15px',
+            color: 'red',
+            margin: '2px 0',
+        },
+    },
+    '.error-mesg': {
+        fontSize: '15px',
+        color: 'red',
+        marginTop: '2px',
+        marginBottom: '0',
+    },
+}));
+
+// 錯誤訊息
+const FormErrorMesg = ({ name, errors }) => (
+
+    <ErrorMessage
+        name={name}
+        errors={errors}
+        render={({ message }) => <p className="error-mesg">{message}</p>}
+        {
+            ...(errors?.[name]?.message === '') && {
+                message: errorMesg[`error_${errors[name]?.type}`],
+            }
+        }
+    />
+
+);
+
+//
 const Login = () => {
+
+    // State
+    const [loading, setLoading] = useState(false);
+
+    //
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm();
+
+    // 送資料
+    const handleReqData = (reqData) => {
+
+        setLoading(true);
+        let auth = btoa(`${reqData.email}:${reqData.password}`);
+
+        Service.login({ headers: { Authorization: `Basic ${auth}`} })
+            .then(() => {
+
+                alert('登入成功，你將被導回首頁!');
+                redirect('/');
+
+            });
+
+    };
 
     return (
 
-        <p>This is login page</p>
+        <MainLayout>
+            <ContentHeader title="登入" />
+
+            <FormLayout onSubmit={handleSubmit(handleReqData)}>
+                <FormRow className={errors?.email?.type ? 'hasError' : ''}>
+                    <div className="title">帳號 (必填)</div>
+                    <div className="field">
+                        <input
+                            type="text"
+                            name="email"
+                            {...register('email', {
+                                required: true,
+                                pattern: {
+                                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g,
+                                    message: errorMesg.error_pattern,
+                                },
+                            })}
+                        />
+                    </div>
+                    <FormErrorMesg
+                        name="email"
+                        errors={errors}
+                    />
+                </FormRow>
+
+                <FormRow className={errors?.password?.type ? 'hasError' : ''}>
+                    <div className="title">密碼 (必填)</div>
+                    <div className="field">
+                        <input
+                            type="password"
+                            name="password"
+                            {...register('password', { required: true })}
+                        />
+                    </div>
+                    <FormErrorMesg
+                        name="password"
+                        errors={errors}
+                    />
+                </FormRow>
+
+                <Buttons
+                    {...loading && { loading }}
+                    text="送出"
+                    htmlType="submit"
+                />
+            </FormLayout>
+        </MainLayout>
 
     );
 
